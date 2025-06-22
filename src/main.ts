222d42267f6c2e810e00623c8b8a4c0afc1616b5
Yevhenns <gifestum@gmail.com>
2025-06-22 13:01:59 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT ?? 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Nostra pizza API Documentation')
+    .setVersion('1.0')
+    .setDescription('Description')
+    .addServer(`http://localhost:${PORT}/api`, 'Development server')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(PORT);
 }
 void bootstrap();
