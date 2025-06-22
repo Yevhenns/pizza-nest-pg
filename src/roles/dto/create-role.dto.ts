8afc25a104e09b29aafe71ca9735d3323233acf1
Yevhenns <gifestum@gmail.com>
2025-06-22 14:12:18 +0300

    added create dto

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,23 @@
-export class CreateRoleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { UserRole } from '../interfaces/role.interface';
+
+export class CreateRoleDto {
+  @ApiProperty({
+    description: 'Name of the user role',
+    example: UserRole.ADMIN,
+    required: true,
+  })
+  @IsEnum(UserRole)
+  @IsNotEmpty()
+  name: UserRole;
+
+  @ApiProperty({
+    description: 'Description of the user role',
+    example: 'Administrator can manage all aspects of the system',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
