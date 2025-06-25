a587f7057ac2ce42dd5c56e745f6dcd577e6eb6e
Yevhenns <gifestum@gmail.com>
2025-06-25 20:26:44 +0300

    type orm module

--- src/admin/categories/categories.module.ts
+++ src/admin/categories/categories.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Product } from '../products/entities/product.entity';
+import { Supplement } from '../supplements/entities/supplement.entity';
+import { ProductsModule } from '../products/products.module';
+import { SupplementsModule } from '../supplements/supplements.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Category, Product, Supplement]),
+    ProductsModule,
+    SupplementsModule,
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
