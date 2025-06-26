9895d03d5441609f5c1f8572240cd271f12e5060
Yevhenns <gifestum@gmail.com>
2025-06-26 10:34:42 +0300

    refactoring roles

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
+import { RolesController } from './controllers/roles.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { RolesService } from './services/roles.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
--- src/roles/services/roles.service.ts
+++ src/roles/services/roles.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Role } from './entities/role.entity';
+import { Role } from '../entities/role.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
