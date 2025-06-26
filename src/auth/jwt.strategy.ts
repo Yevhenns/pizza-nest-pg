678849de50a393c2d4e5a4f9a61580756bb35b98
Yevhenns <gifestum@gmail.com>
2025-06-26 09:08:01 +0300

    added guard

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { CustomJwtPayload } from './interfaces/auth.interface';
+dotenv.config();
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  validate(payload: CustomJwtPayload) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
