f87fc4401aea727111ba53a7af23d9e87e7bbd6a
Yevhenns <gifestum@gmail.com>
2025-06-28 19:17:51 +0300

    edit user

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SupplementsModule } from './supplements/supplements.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, SupplementsModule],
+  imports: [ProductsModule, CategoriesModule, SupplementsModule, UsersModule],
 })
 export class AdminModule {}
