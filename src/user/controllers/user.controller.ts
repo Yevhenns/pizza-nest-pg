0b5f1c9df90f001d7c87f6499769634947258dca
Yevhenns <gifestum@gmail.com>
2025-07-02 20:15:05 +0300

    update user

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -6,10 +6,17 @@ import {
   Param,
   UseGuards,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserService } from '../services/user.service';
-import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '~/auth/guards/jwt-auth.guard';
 import { CreateOrderMailDto } from '~/order-mail/dto/create-order-mail.dto';
 import { CurrentUser } from '../decorators/user.decorator';
@@ -17,6 +24,7 @@ import { CustomJwtPayload } from '~/auth/interfaces/auth.interface';
 import { ToUserDto } from '../dto/to-user.dto';
 import { ChangePasswordDto } from '../dto/change-password.dto';
 import { SuccessDto } from '~/dto/success.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
 export class UserController {
@@ -57,11 +65,14 @@ export class UserController {
     description: 'Returns updated user',
     type: ToUserDto,
   })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('avatar'))
   update(
     @CurrentUser() user: CustomJwtPayload,
     @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile() avatar: Express.Multer.File,
   ) {
-    return this.userService.update(user, updateUserDto);
+    return this.userService.update(user, updateUserDto, avatar);
   }
 
   @Patch('change_password')
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,11 @@ import { UserService } from './services/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserOrder } from '~/order-mail/entities/order-mail.entity';
 import { User } from './entities/user.entity';
+import { CloudinaryService } from '~/cloudinary/cloudinary.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserOrder, User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, CloudinaryService],
 })
 export class UserModule {}
